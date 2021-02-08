936aa23caea53acf8c78bdeb178e3360fbc0874c
김은혜 <jailke93@gmail.com>
2021-02-08 15:59:28 +0900

    game: JS popup.js

--- CH4_game/src/main.js
+++ CH4_game/src/main.js
@@ -1,4 +1,5 @@
 "use strict";
+import PopUp from "./popup.js";
 
 const CARROT_SIZE = 80;
 const CARROT_COUNT = 5;
@@ -12,10 +13,6 @@ const gameBtn = document.querySelector(".game__button");
 const gameTimer = document.querySelector(".game__timer");
 const gameScore = document.querySelector(".game__score");
 
-const popUp = document.querySelector(".pop-up");
-const popUpText = document.querySelector(".pop-up__message");
-const popUpRefresh = document.querySelector(".pop-up__refresh");
-
 const carrotSound = new Audio("./sound/carrot_pull.mp3");
 const alertSound = new Audio("./sound/alert.wav");
 const bgSound = new Audio("./sound/bg.mp3");
@@ -26,6 +23,10 @@ let started = false;
 let score = 0;
 let timer = undefined;
 
+const gameFinishBanner = new PopUp();
+gameFinishBanner.setClickListener(() => {
+  startGame();
+});
 field.addEventListener("click", onFieldClick);
 
 gameBtn.addEventListener("click", () => {
@@ -34,11 +35,6 @@ gameBtn.addEventListener("click", () => {
   } else {
     startGame();
   }
-});
-
-popUpRefresh.addEventListener("click", () => {
-  startGame();
-  hidePopUp();
 });
 
 function startGame() {
@@ -54,7 +50,7 @@ function stopGame() {
   started = false;
   stopGameTimer();
   hideGameButton();
-  showPopUpWithText("REPLAY?");
+  gameFinishBanner.showWithText("Replay?");
   playSound(alertSound);
   stopSound(bgSound);
 }
@@ -69,7 +65,7 @@ function finishGame(win) {
   }
   stopGameTimer();
   stopSound(bgSound);
-  showPopUpWithText(win ? "YOU WON" : "YOU LOST");
+  gameFinishBanner.showWithText(win ? "YOU WON" : "YOU LOST");
 }
 
 function showStopButton() {
@@ -104,16 +100,7 @@ function startGameTimer() {
 function stopGameTimer() {
   clearInterval(timer);
   hideGameButton();
-  showPopUpWithText("Replay?");
-}
-
-function showPopUpWithText(text) {
-  popUpText.innerText = text;
-  popUp.classList.remove("pop-up--hide");
-}
-
-function hidePopUp() {
-  popUp.classList.add("pop-up--hide");
+  gameFinishBanner.showPWithText("Replay?");
 }
 
 function updateTimerText(time) {
